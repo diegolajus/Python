4da3030e669491a9c45a11df5c340bf4789ca2a8
diegolajus <dielasu@gmail.com>
2021-08-12 20:15:11 +0200

    Quest 3

--- Practice/FruitsPrices.py
+++ Practice/FruitsPrices.py
@@ -8,26 +8,20 @@ lista_precios_kg = {
 
 
 
-#Mostrar al usuario qué fruta hay disponible (Se muestra como una lista)
 # Funcion para devolver la lista en string (Visualmente mejor en la consola después)     
 def listToString(fruta_disponible): 
     str1 = " " 
+    return (str1.join(fruta_disponible))  
 
-    return (str1.join(fruta_disponible))
-           
+#Mostrar al usuario qué fruta hay disponible (Se muestra como una lista)
 fruta_disponible = [key for key in lista_precios_kg.keys()]
-
-
-print(f"Hoy nos queda esta fruta: ", listToString(fruta_disponible)) 
-
-
-
+print(f"Hoy nos queda esta fruta: ", listToString(fruta_disponible))
 
 # Pedimos al usuario qué fruta quiere comprar
 fruta = input ("¿Qé fruta quieres? ")
 
 if fruta in lista_precios_kg:
-    kilos = float(input(f"¿Cuantos kilos de {fruta} quieres?"))
-    print(f"{kilos}Kg de {fruta} valen a {lista_precios_kg[fruta]*kilos} $ ")
+    kilos = float(input(f"¿Cuantos kilos de {fruta} quieres?")) # Le preguntamos cuantos Kg quiere
+    print(f"{kilos}Kg de {fruta} valen a {lista_precios_kg[fruta]*kilos} $ ") # il importe queda calculadd y se muestra por pantalla
 else:
-    print(f"{fruta} no se encuentra disponible")
+    print(f"{fruta} no se encuentra disponible") # En caso de error saldrá que no esta disponible.
