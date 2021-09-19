4708b5a8c1fe515a7f7e5d63bc1b4e3284634b66
diegolajus <dielasu@gmail.com>
2021-09-19 18:45:09 +0200

    Objects"

--- Practice/CatObjects.py
+++ Practice/CatObjects.py
@@ -6,17 +6,13 @@ class Cat:
         self.age = age
 
 # 1 Instantiate the Cat object with 3 cats
-
 cat1 = Cat("Spark",2) 
 cat2 = Cat("Mike",9) 
 cat3 = Cat("Toby",3) 
 
 # 2 Create a function that finds the oldest cat
-cats = [cat1,cat2,cat3]
-# max_age = max(cats.age)
-max_age = max(cat1.age,cat2.age,cat3.age) 
+max_age = max(cat1.age,cat2.age,cat3.age)
 
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
-
 print (f" The oldest cat is {max_age} years old")
