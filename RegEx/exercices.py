ce0a31db66495a3f830e771b934cee95f31af4fc
dielasu <dielasu@gmail.com>
2021-12-02 02:31:26 +0100

    RegEx2

--- RegEx/exercices.py
+++ RegEx/exercices.py
@@ -8,7 +8,23 @@ from typing import Text
 # MATCH TEXT ?=+. ✔
 # SKIP TEXT abc1  ✖
 
-strings="cat. 896. ?=+. abc1"
+ex1="cat. 896. ?=+. abc1"
 patt = re.compile(r".+\.")
-ex_1 = patt.findall(strings)
+ex_1 = patt.findall(ex1)
 print(ex_1)
+
+
+# MATCH TEXT can ✔
+# MATCH TEXT man ✔
+# MATCH TEXT fan ✔
+# SKIP TEXT dan  ✖
+# SKIP TEXT ran  ✖
+# SKIP TEXT pan  ✖
+
+ex2="can man fan dan ran pan"
+patt = re.compile(r"[cmf]an")
+ex_2 = patt.findall(ex2)
+print(ex_2)
+
+
+# [cmf] -> it will only find the words starting from c, m, f. can, man and fan will match here
