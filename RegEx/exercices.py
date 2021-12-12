7ea979f5f7affc1bd5afda49022e062babca1580
dielasu <dielasu@gmail.com>
2021-12-12 17:39:34 +0100

    RegEx7

--- RegEx/exercices.py
+++ RegEx/exercices.py
@@ -73,3 +73,18 @@ def text_match(text):
 print(text_match("ab"))
 print(text_match("abc"))
 print(text_match("aBc"))
+
+
+# 6 Write a Python program that matches a string that has an a followed by zero or more b's.
+
+def text_match(text):
+        patterns = 'ab+?'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+print(text_match("ab"))
+print(text_match("abc"))
+print(text_match("aBc"))
+
