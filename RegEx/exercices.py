d754e8fe6d05e4517f037060885146fd9e8c6e20
dielasu <dielasu@gmail.com>
2021-12-16 01:23:28 +0100

    RegEx9

--- RegEx/exercices.py
+++ RegEx/exercices.py
@@ -97,3 +97,16 @@ print("Ex8 i) -->",longest_item('Red', 'Green', 'Black', 'Orange'))
 print("Ex8 ii) -->",longest_item([1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]))
 print("Ex8 iii) -->",longest_item([1, 2, 3], 'Java'))
 print("Ex8 iv) -->",longest_item({10, 100}, 'Python'))
+
+# 9 Write a Python program that matches a string that has an a followed by three 'b'.
+
+import re
+def text_match(text):
+        patterns = 'ab{3}?'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+print("Ex9 i) -->",text_match("abbb"))
+print("Ex9 i) -->",text_match("aabbbbbc"))
