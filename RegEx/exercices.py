0482a44cf85eb9cea09d553fb3ebb004ad6f5230
dielasu <dielasu@gmail.com>
2021-12-28 00:54:50 +0100

    RegEx15

--- RegEx/exercices.py
+++ RegEx/exercices.py
@@ -181,3 +181,25 @@ def text_match(text):
 
 print("Ex14 i) -->",text_match("The quick brown fox jumps over the lazy dog."))
 print("Ex14 ii) -->",text_match(" The quick brown fox jumps over the lazy dog."))
+
+
+# 15 Write a Python program that matches a word at the end of a string, with optional punctuation.
+
+def text_match(text):
+        patterns = '^\w+'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+import re
+def text_match(text):
+        patterns = '\w+\S*$'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+print("Ex15 i) -->",text_match("The quick brown fox jumps over the lazy dog."))
+print("Ex15 ii) -->",text_match("The quick brown fox jumps over the lazy dog. "))
+print("Ex15 iii) -->",text_match("The quick brown fox jumps over the lazy dog "))
